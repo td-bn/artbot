cfc02c9647b732f1f172bbcdb0f94544f189ba93
td-bn <bnabi@protonmail.com>
2021-11-24 14:25:23 +0530

    first draft of arb code

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -1,8 +1,12 @@
-import { Contract, ethers } from 'ethers';
+import { Contract, ethers, Signer } from 'ethers';
 import {abi, address as depAddress} from '../../deployments/localhost/ArBot.json'
 import uniswapV2 from '@studydefi/money-legos/uniswapV2'
+import ERC20 from '@studydefi/money-legos/erc20/abi/ERC20.json'
+import { tokens } from './config';
 
 const provider = new ethers.providers.JsonRpcProvider()
+
+export const getSigner = (): Signer => provider.getSigner()
 
 export const getContractInstance = (): Contract => {
   const contract = new ethers.Contract(depAddress, abi, provider)
@@ -13,4 +17,8 @@ export const getV2PairInstance = (address: string): Contract => {
   const abi = uniswapV2.pair.abi
   const contract = new ethers.Contract(address, abi, provider)
   return contract
+}
+
+export const getDaiInstance = (): Contract => {
+  return new ethers.Contract(tokens.DAI, ERC20, provider)
 }
